60555bf65486980a9370099cc3d5d53abbe293b6
Adam Neuber <adam.neuber.024@pslib.cz>
2025-01-10 17:27:18 +0100

    Gameplay Added

--- main.ts
+++ main.ts
@@ -1,10 +1,31 @@
-//Repozitář pro odevzdání: https://classroom.github.com/a/IYwiG2Dr
+let minNum = 1
+let maxnuM = 99
+let tries = 1
+let random = randint(minNum,maxnuM)
 
-
-const minNum = 1
-const maxnuM = 100
 basic.showString("ButtonA - Too High, B - Too low, A+B - Correct")
 
+whaleysans.showNumber(random)
+
 basic.forever(function() {
-    
+    input.onButtonPressed(Button.A, function() {
+        maxnuM = random - 1
+        random = randint(minNum, maxnuM)
+        whaleysans.showNumber(random)
+        tries+=1
+    })
+
+    input.onButtonPressed(Button.B, function () {
+        minNum = random + 1
+        random = randint(minNum, maxnuM)
+        whaleysans.showNumber(random)
+        tries+=1
+    })
+
+    input.onButtonPressed(Button.AB, function () {
+        basic.showString(" Tries")
+        basic.showNumber(tries)
+        basic.pause(3000)
+        control.reset()
+    })
 })
